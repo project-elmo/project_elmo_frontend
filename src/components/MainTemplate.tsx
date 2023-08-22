7bc6a2c8c5d2f8abae6083f2339026ce8a20b923
hhkim0729 <hhkim0729@gmail.com>
2023-08-22 17:30:55 +0900

    chore: Modify main max width

--- src/components/MainTemplate.tsx
+++ src/components/MainTemplate.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function MainTemplate({ title, children }: Props) {
   return (
     <main className="flex-1 pt-8 px-8 md:pt-20 md:pl-14">
-      <div className="max-w-screen-xl m-auto">
+      <div className="max-w-screen-md m-auto">
         <h2>{title}</h2>
         {children}
       </div>
