84cdaaf009380c3b869d95d403355475b6198384
hhkim0729 <hhkim0729@gmail.com>
2023-08-23 00:41:03 +0900

    feat: Add setting page(#11)

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,3 +1,25 @@
+import Button from '@/components/Button';
+import MainTemplate from '@/components/MainTemplate';
+import TextInputWithLabel from '@/components/TextInputWithLabel';
+import { SERVICE_NAME } from '@/constants';
+
 export default function Setting() {
-  return <div>Setting</div>;
+  return (
+    <MainTemplate
+      title="Environment Setting"
+      description={`You can change these values at any time in the 'Menu > Environment Settings'.`}
+    >
+      <form>
+        <TextInputWithLabel
+          id="server-url"
+          label="Server URL"
+          info="example information"
+          placeholder={`http://localhost:8080`}
+        />
+        <div className="mt-6 text-center">
+          <Button type="submit">Start {SERVICE_NAME}</Button>
+        </div>
+      </form>
+    </MainTemplate>
+  );
 }
