aca069da862b6a1a8f6831a2929ac5babab523f0
hhkim0729 <hhkim0729@gmail.com>
2023-09-06 20:52:38 +0900

    feat: Add parameter infos(#124)

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -17,7 +17,7 @@ export default function Tooltip({ info }: TooltipProps) {
         </TooltipTrigger>
         <TooltipPrimitive.Content
           sideOffset={4}
-          className="inline-flex items-center border shadow shadow-line/30 rounded-md bg-white px-4 py-2.5 text-xs"
+          className="max-w-sm inline-flex items-center border shadow shadow-line/30 rounded-md bg-white px-4 py-2.5 text-xs whitespace-pre-line"
         >
           <p>{info}</p>
         </TooltipPrimitive.Content>
