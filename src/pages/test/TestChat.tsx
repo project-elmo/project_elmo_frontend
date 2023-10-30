c1c41c0b95b518c433768f2bf707e3eb9583b9c1
hhkim0729 <hhkim0729@gmail.com>
2023-10-30 21:46:35 +0900

    feat: Add uploading pdf while testing(#156)

--- src/pages/test/TestChat.tsx
+++ src/pages/test/TestChat.tsx
@@ -2,15 +2,12 @@ import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createMessage } from '@/api/rest';
-import { MdOutlineChevronLeft, MdOutlineClose, MdSend } from 'react-icons/md';
+import { MdSend } from 'react-icons/md';
 import Chat from '@/components/test/Chat';
 import Button from '@/components/Button';
 import Textarea from '@/components/Textarea';
-import SideNav from '@/components/SideNav';
-import SliderWithLabel from '@/components/SliderWithLabel';
-import TextInputWithLabel from '@/components/TextInputWithLabel';
-import { formatNumber } from '@/utils';
-import { QUERY_KEYS, INFOS } from '@/constants';
+import ParamNav from '@/components/test/ParamNav';
+import { QUERY_KEYS } from '@/constants';
 import { TestMessage, TestMessageForm } from '@/types';
 
 interface Props {
@@ -36,6 +33,8 @@ export default function TestChat({ testNos }: Props) {
     top_p: 1,
     repetition_penalty: 1,
     no_repeat_ngram_size: '0',
+    pdf_file_name: '',
+    lang: 'eng',
   });
   const queryClient = useQueryClient();
   const chatQueryKey = QUERY_KEYS.CHAT_HISTORY;
@@ -124,116 +123,7 @@ export default function TestChat({ testNos }: Props) {
           </div>
         </form>
       </div>
-      <ParamsNav formData={formData} setFormData={setFormData} />
+      <ParamNav formData={formData} setFormData={setFormData} />
     </main>
-  );
-}
-
-interface ParamsNavProps {
-  formData: TestMessageForm;
-  setFormData: React.Dispatch<React.SetStateAction<TestMessageForm>>;
-}
-
-function ParamsNav({ formData, setFormData }: ParamsNavProps) {
-  const [showParamsNav, setShowParamsNav] = useState(true);
-  return (
-    <>
-      {showParamsNav ? (
-        <SideNav side="right">
-          <div className="text-right">
-            <Button listStyle onClick={() => setShowParamsNav(false)}>
-              <MdOutlineClose />
-            </Button>
-          </div>
-          <div className="p-4">
-            <SliderWithLabel
-              id="maximum-length"
-              label="Maximum Length"
-              info={INFOS.MAXIMUM_LENGTH}
-              value={formData.max_length}
-              max={512}
-              min={10}
-              onValueChange={(value) =>
-                setFormData((prev) => ({ ...prev, max_length: value[0] }))
-              }
-            />
-            <SliderWithLabel
-              id="temperature"
-              label="Temperature"
-              info={INFOS.TEMPERATURE}
-              value={formData.temperature}
-              max={2}
-              step={0.1}
-              onValueChange={(value) =>
-                setFormData((prev) => ({ ...prev, temperature: value[0] }))
-              }
-            />
-            <TextInputWithLabel
-              id="top-k"
-              label="Top K"
-              info={INFOS.TOP_K}
-              value={formData.top_k}
-              className="mb-4"
-              onChange={({ target }) =>
-                setFormData((prev) => ({
-                  ...prev,
-                  top_k: formatNumber(target.value),
-                }))
-              }
-            />
-            <SliderWithLabel
-              id="top-p"
-              label="Top P"
-              info={INFOS.TOP_P}
-              value={formData.top_p}
-              max={2}
-              step={0.1}
-              onValueChange={(value) =>
-                setFormData((prev) => ({ ...prev, top_p: value[0] }))
-              }
-            />
-            <SliderWithLabel
-              id="repetition-penalty"
-              label="Repetition Penalty"
-              info={INFOS.REPETITION_PENALTY}
-              value={formData.repetition_penalty}
-              min={1}
-              max={2}
-              step={0.1}
-              onValueChange={(value) =>
-                setFormData((prev) => ({
-                  ...prev,
-                  repetition_penalty: value[0],
-                }))
-              }
-            />
-            <TextInputWithLabel
-              id="no-repeat-ngram-size"
-              label="No Repeat N-gram Size"
-              info={INFOS.NO_REPEAT_NGRAM_SIZE}
-              value={formData.no_repeat_ngram_size}
-              className="mb-4"
-              onChange={({ target }) =>
-                setFormData((prev) => ({
-                  ...prev,
-                  no_repeat_ngram_size: formatNumber(target.value),
-                }))
-              }
-            />
-          </div>
-        </SideNav>
-      ) : (
-        <Button
-          listStyle
-          onClick={() => setShowParamsNav(true)}
-          className="absolute right-0 m-1.5 z-10 shadow-md pr-5"
-        >
-          <div className="flex gap-1">
-            <MdOutlineChevronLeft />
-            Params
-          </div>
-        </Button>
-      )}
-    </>
   );
 }
