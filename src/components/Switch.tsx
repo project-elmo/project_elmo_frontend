43e7a1373ab213cd68a1745ae90fe53c0ce11b7e
hhkim0729 <hhkim0729@gmail.com>
2023-10-30 21:41:29 +0900

    chore: Make background white

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -20,7 +20,7 @@ export default function Switch({
       checked={checked}
       disabled={disabled}
       onCheckedChange={onCheckedChange}
-      className="radix-state-checked:border-primary relative inline-flex items-center h-6 w-10 rounded-full border-2 border-line transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring focus-visible:ring-primary radix-disabled:cursor-not-allowed"
+      className="radix-state-checked:border-primary relative inline-flex items-center h-6 w-10 rounded-full border-2 border-line bg-white transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring focus-visible:ring-primary radix-disabled:cursor-not-allowed"
     >
       <SwitchThumb className="radix-state-checked:bg-primary radix-state-checked:translate-x-4 radix-state-unchecked:translate-x-0 pointer-events-none inline-block h-5 w-5 border-2 border-white transform rounded-full bg-line shadow-lg ring-0 transition duration-200 ease-in-out radix-disabled:cursor-not-allowed" />
     </SwitchRoot>
